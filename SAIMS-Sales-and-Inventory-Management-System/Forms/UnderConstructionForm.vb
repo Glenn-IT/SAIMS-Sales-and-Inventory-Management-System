3565934bff7e958e06ed548b570a3d506a8912a9
Glenn-IT <glenard2308@gmail.com>
2026-07-06 20:29:22 +0800

    feat: implement v1.07 - unlock Sales Transaction

--- SAIMS-Sales-and-Inventory-Management-System/Forms/UnderConstructionForm.vb
+++ SAIMS-Sales-and-Inventory-Management-System/Forms/UnderConstructionForm.vb
@@ -1,6 +1,6 @@
 Public Class UnderConstructionForm
 
-    Public Const CURRENT_VERSION As String = "v1.06"
+    Public Const CURRENT_VERSION As String = "v1.07"
 
     Private Sub UnderConstructionForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblVersion.Text = "Current Version: " & CURRENT_VERSION
